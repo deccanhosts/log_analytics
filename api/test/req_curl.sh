970d0cc13541ef37f79e63381cb5b349a79e79b6
deccanfoo <deccanfoo@deccanhosts.com>
2012-11-03 23:46:12 -0700

    test template

---
+++ api/test/req_curl.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -i -H "Accept: application/json" -H "Content-type: application/octet-stream" -X POST --data-binary @sample_data.txt localhost:8086/api/v0/userenv/ -vvv
+
---
+++ api/test/test_get.py
@@ -0,0 +1,8 @@
+import log_analytics_proto
+
+def buildProto():
+ """Test protocol buffer serialization."""
+
+if __name__ == "__main__":
+    buildProto()
+
