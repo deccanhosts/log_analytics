ec66dc720404ca08e56cf188acdd927eccdca5cb
deccanfoo <deccanfoo@deccanhosts.com>
2012-12-10 00:05:55 -0800

    fix test

--- api/test/test_get.py
+++ api/test/test_get.py
@@ -11,7 +11,7 @@ req_msg = log_analytics_proto.req_msg(req_id      = "test_id",\
 wire_str = req_msg.SerializeToString()
 wire_str_enc = base64.b64encode(wire_str)
 print "serialized request:: \n", wire_str_enc
-open("tmp_get_test.txt", "wb").write(wire_str)
+open("tmp_get_test.txt", "wb").write(wire_str_enc)
 
 if __name__ == "__main__":
   buildProto()
