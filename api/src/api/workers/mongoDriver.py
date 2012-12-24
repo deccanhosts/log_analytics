3f4d8ccb182fbef2d7ab25453f6b511c575697dc
deccanfoo <deccanfoo@deccanhosts.com>
2012-12-23 20:36:03 -0800

    fix year logic

--- api/src/api/workers/mongoDriver.py
+++ api/src/api/workers/mongoDriver.py
@@ -56,6 +56,7 @@ def getVisitArrAll(vhost = None, modulo = None, startDate = None, endDate = None
   q = db.command('aggregate', config.aplogCollection, pipeline=pipeline)
     
   #q = db.command('aggregate', 'aplogs', pipeline=pipeline, explain = True)
+  print "result... " , q, "********\n"
   return q["result"], ""
 
 def getVisitArrHtml(vhost = None, modulo = None, startDate = None, endDate = None):
